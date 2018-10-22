009f26ae39df86246a6ce3d85971fe958fbc5c80
dukc <ajieskola@gmail.com>
2018-10-22 14:29:22 +0300

    Lisätty kaiverrusvärin muutto.

--- source/app.d
+++ source/app.d
@@ -13,9 +13,60 @@ int main(string[] args)
 {   version (Windows) SetConsoleOutputCP(65001);
     DerelictFI.load();
 
-    if (args.length != 2)
-    {   writeln("Tarvitaan yksi argumentti: tiedoston nimi");
-        return 0;
+    ushort rgbColour = ushort.max;
+    bool marginalsDisliked = false;
+
+    try
+    {   string colourString = "";
+        auto info = getopt
+        (   args,
+            std.getopt.config.stopOnFirstNonOption,
+            "colour|c", "Väri johon muuttaa kuva", &colourString,
+            "marginals|m", "Leikataan marginaalit pois", &marginalsDisliked,
+        );
+
+        if (info.helpWanted)
+        {   stdout.lockingTextWriter.defaultGetoptFormatter
+            (   "Leikkaa ylimääräiset marginaalit ja/tai jälleenvärittää yksiväriset kuvat.\n"
+                ~ text("Käyttö: ", args[0], " argumentit...", " tiedostonnimi\n")
+                ~ "(C) Ylivieskan kivihiomo KY\n\n",
+                info.options
+            );
+
+            return 0;
+        }
+
+        if (args.length < 2)
+        {   writeln("Tiedoston nimi puuttuu");
+            return 0;
+        }
+
+        if (args.length > 2)
+        {   writeln("Ohjelma nielee vain yhden tiedoston nimen");
+            return 0;
+        }
+
+        if (colourString != "" &&
+        (   colourString.length != 3
+            || (colourString).toLower.formattedRead!("%x")(rgbColour) < 1
+        ))
+        {   throw new Exception
+            (   "Värille tai täytön voimalle annettu argumentti väärässä muodossa. Niiden pitää olla 3 ja 1 heksadesimaalinumeroa "
+                ~ " eli numeroa 0-9 tai kirjainta a-f tai A-F. (kirjaimet vastaavat numeroita 10-15)"
+            );
+        }
+
+        if (colourString.empty && !marginalsDisliked)
+        {   writeln("Kutsussa ei ole järkeä, koska ohjelmaa ei komennettu varsinaisesti tekemään mitään.");
+            writeln("Anna ohjelmalle joko -c tai -m - argumentti (tai molemmat).");
+            return 0;
+        }
+    }
+    catch (ConvException e)
+    {   throw new Exception("Argumentille syötetty arvo väärässä muodossa.\n" ~ e.message.idup);
+    }
+    catch (GetOptException e)
+    {   throw new Exception("Tuntematon argumentti.\n" ~ e.message.idup);
     }
 
     auto fileExt = args[1].extension.toLower;
@@ -27,41 +78,47 @@ int main(string[] args)
 
     immutable(wchar)* cPath = (args[$ - 1].to!wstring ~ '\0').toLower.ptr;
 
-    auto bitmap = fileExt.predSwitch
+    auto bitmap0 = fileExt.predSwitch
     (   ".gif", FreeImage_LoadU(FIF_GIF, cPath, GIF_PLAYBACK),
         ".png", FreeImage_LoadU(FIF_PNG, cPath, 0           ),
     );
 
-    if (bitmap is null)
+    if (bitmap0 is null)
     {   writeln("Tiedosto ei auennut. Tarkista polku ja, jos se on kunnossa, käyttöoikeudet.");
         return 0;
     }
-    scope (exit) bitmap.FreeImage_Unload;
+    scope (exit) bitmap0.FreeImage_Unload;
 
-    if(!bitmap.FreeImage_HasPixels)
+    if(!bitmap0.FreeImage_HasPixels)
     {   writeln("Tyhjä kuvatiedosto. Ei kelpaa.");
         return 0;
     }
 
-    auto newBitmap = bitmap.cutMarginals.visit!
+    auto finalBitmap = marginalsDisliked? bitmap0.cutMarginals.visit!
     (   (Bitmap bm) => bm,
         (string msg)
         {   msg.writeln;
             return Bitmap(null);
         }
+    ): bitmap0;
+    if (finalBitmap == null) return 0;
+    scope (exit) finalBitmap.FreeImage_Unload;
+
+    if(rgbColour < 0x1000) finalBitmap.colourize
+    (     finalBitmap.FreeImage_GetRedMask   / 0xF * (rgbColour / 0x100 & 0xF)
+        | finalBitmap.FreeImage_GetGreenMask / 0xF * (rgbColour / 0x010 & 0xF)
+        | finalBitmap.FreeImage_GetBlueMask  / 0xF * (rgbColour / 0x001 & 0xF)
     );
-    if (newBitmap == null) return 0;
-    scope (exit) newBitmap.FreeImage_Unload;
 
     if(fileExt.predSwitch
     (   ".gif", ()
-        {   auto palettized = newBitmap.FreeImage_ColorQuantize(FIQ_WUQUANT);
+        {   auto palettized = finalBitmap.FreeImage_ColorQuantize(FIQ_WUQUANT);
             assert(palettized != null);
             scope (exit) palettized.FreeImage_Unload;
 
             //palettisointi ei jostain syystä säästä kuvan läpinäkyvyyttä, joten asetetaan se itse.
-            CoordinateInt[2] transparentIj = newBitmap.transparentCoord;
-            if (transparentIj[1] < bitmap.FreeImage_GetHeight)
+            CoordinateInt[2] transparentIj = finalBitmap.transparentCoord;
+            if (transparentIj[1] < finalBitmap.FreeImage_GetHeight)
             {   ubyte transparentColour;
                 auto success = palettized.FreeImage_GetPixelIndex(transparentIj.tuplify.expand, &transparentColour);
                 assert(success);
@@ -70,27 +127,22 @@ int main(string[] args)
 
             return FreeImage_SaveU(FIF_GIF, palettized, cPath, 0);
         }(),
-        ".png", FreeImage_SaveU(FIF_PNG, newBitmap, cPath, 0)
-    )) writeln("Onnistui, marginaalit leikattu");
-    else writeln("Ohjelma avasi kuvan ja leikkasi marginaalit, muttei jostain syystä pystynyt tallentamaan tulosta.");
+        ".png", FreeImage_SaveU(FIF_PNG, finalBitmap, cPath, 0)
+    )) writeln("Ohjelma ajettu onnistuneesti.");
+    else writeln("Ohjelma avasi kuvan ja teki operaatiot, muttei jostain syystä pystynyt tallentamaan tulosta.");
 
     return 0;
 }
 
 Algebraic!(Bitmap, string) cutMarginals(Bitmap bitmap)
-{   CoordinateInt pixelBits = bitmap.FreeImage_GetBPP;
+{   writeln("Karsitaan marginaaleja");
+    CoordinateInt pixelBits = bitmap.FreeImage_GetBPP;
 
     auto dimensions = [bitmap.FreeImage_GetWidth, bitmap.FreeImage_GetHeight].staticArray;
     auto sideCoords = [dimensions[0], 0].staticArray!CoordinateInt;
     auto bottomUpCoords = [dimensions[1], 0].staticArray!CoordinateInt;
 
     assert(bitmap.FreeImage_GetBPP / 8 == Pixel.sizeof);
-    /*auto transparentColours = bitmap
-        .FreeImage_GetTransparencyTable[0 .. bitmap.FreeImage_GetTransparencyCount]
-        .uniq
-        .array
-        .pipe!(a => a ~ *bitmap.FreeImage_GetScanLine(0))
-        .sort;*/
 
     auto alphaMask = ~
     (   bitmap.FreeImage_GetRedMask   |
@@ -125,7 +177,7 @@ Algebraic!(Bitmap, string) cutMarginals(Bitmap bitmap)
     auto newDimensions = [sideCoords[1] - sideCoords[0], bottomUpCoords[1] - bottomUpCoords[0]].staticArray;
 
     if (sideCoords[1] <= sideCoords[0])
-    {   return typeof(return)("Koko kuva on täysin läpinäkyvä. Ei jäisi mitään jäljelle, joten ei leikata.");
+    {   return typeof(return)("Koko kuva on täysin läpinäkyvä. Marginaalileikkaus ei jättäisi mitään jäljelle, joten ohjelma ei tehnyt mitään.");
     }
     assert(bottomUpCoords[1] > bottomUpCoords[0]);
 
@@ -162,6 +214,22 @@ CoordinateInt[2] transparentCoord(Bitmap bitmap)
     .chain([0, dimensions[1]].staticArray.only)
     .front
     ;
+}
+
+void colourize(Bitmap bitmap, Pixel how)
+{   writeln("vaihdetaan väriä");
+    auto dimensions = [bitmap.FreeImage_GetWidth, bitmap.FreeImage_GetHeight].staticArray;
+
+    auto alphaMask = ~
+    (   bitmap.FreeImage_GetRedMask   |
+        bitmap.FreeImage_GetGreenMask |
+        bitmap.FreeImage_GetBlueMask
+    );
+
+    foreach(lineY; iota(dimensions[1]))
+        foreach(ref px; (cast(Pixel*) bitmap.FreeImage_GetScanLine(lineY))[0 .. to!CoordinateInt(dimensions[0])])
+    {   px = px & alphaMask | how & ~alphaMask;
+    }
 }
 
 ////////////////////////////////////////////
